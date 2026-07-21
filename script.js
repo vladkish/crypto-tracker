67b7b372e6de84352d5e74d408d728b2002c70d6
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-07-21 16:56:16 +0200

    renderCoin

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log(true);
+// data.
+const porfolio = [];
+const nameAllCoint = [];
+
+function renderCoin() {
+
+    const form = document.querySelector('#form');
+    
+    form.addEventListener('submit', (e) => {
+        const coint = {};
+        e.preventDefault();
+
+        if (!nameAllCoint.includes(document.querySelector(`[date-input="1"]`).value)) {
+
+            coint['nameCoin'] = document.querySelector(`[date-input="1"]`).value;
+            coint['countCoin'] = document.querySelector(`[date-input="2"]`).value;
+            coint['priceCoin'] = document.querySelector(`[date-input="3"]`).value;
+            coint['waitPriceCoin'] = document.querySelector(`[date-input="4"]`).value;
+
+            porfolio.push(coint);
+            nameAllCoint.push(document.querySelector(`[date-input="1"]`).value);
+        }
+
+        console.log(porfolio);
+        form.reset();
+    });
+
+}
+
+renderCoin();
