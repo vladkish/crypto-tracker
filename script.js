e7035c87e93c63af78526c6864477bc35bc00824
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-07-21 17:54:22 +0200

    rendering portfolio

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
 'use strict';
 
+// data.
 const PortfolioApp = {
-    porfolio : [],
-    'render' : renderCoin,
-    nameAllCoint : [],
+    porfolio: [],
+    'renderCoin': renderCoin,
+    'renderPortfolio': renderPortfolio,
+    nameAllCoint: [],
 };
 
-function renderCoin() {
+function renderPortfolio () {
 
+    for (const value of this.porfolio) {
+
+        // block.
+        const block = document.createElement("div");
+        block.classList.add('card-coin');
+        document.querySelector('.coin-section').append(block);
+
+        // h1.
+        const textBlock = document.createElement('h1');
+        textBlock.classList.add('name-coin');
+        textBlock.textContent = value.nameCoin;
+        block.append(textBlock);
+
+        // price.
+        const price = document.createElement('p');
+        price.classList.add('price');
+        price.textContent = value.priceCoin;
+        block.append(price);
+
+        // count.
+        const count = document.createElement('p');
+        count.classList.add('count-coin');
+        count.textContent = value.countCoin;
+        block.append(count);
+
+        // plus.
+        const minus = document.createElement('p');
+        minus.classList.add('minus-coin');
+        minus.textContent = '-----';
+        block.append(minus);
+
+        // minus.
+        const plus = document.createElement('p');
+        plus.classList.add('plus-coin');
+        plus.textContent = '-----';
+        block.append(plus);
+    }
+}
+
+// form.
+function renderCoin() {
     const form = document.querySelector('#form');
-    
+
     form.addEventListener('submit', (e) => {
 
-        const {porfolio, nameAllCoint} = PortfolioApp;
+        const { porfolio, nameAllCoint } = PortfolioApp;
 
         const coint = {};
         e.preventDefault();
@@ -29,8 +72,8 @@ function renderCoin() {
         }
 
         form.reset();
+        PortfolioApp.renderPortfolio();
     });
-}
 
-PortfolioApp['render']();
-console.log(PortfolioApp.porfolio);
+}
+renderCoin();
