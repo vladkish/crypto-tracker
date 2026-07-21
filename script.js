9abfbd97aedbfe51cc9c9e6e9cf4e3812006ca40
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-07-21 18:50:27 +0200

    fix bug about delete object

--- script.js
+++ script.js
@@ -22,59 +22,48 @@ function renderRemove() {
 // add object.
 function renderPortfolio () {
 
-    const proverka = (this.porfolio.countCoin == '' 
-        || this.porfolio.priceCoin == '' 
-        || this.porfolio.nameCoin == '' 
-        || this.porfolio.waitPriceCoin == '');
+    for (const value of this.porfolio) {
 
-    console.log(proverka);
+        // block.
+        const block = document.createElement("div");
+        block.classList.add('card-coin');
+        document.querySelector('.coin-section').append(block);
 
-    if (proverka) {
-        for (const value of this.porfolio) {
+        // h1.
+        const textBlock = document.createElement('h1');
+        textBlock.classList.add('name-coin');
+        textBlock.textContent = value.nameCoin;
+        block.append(textBlock);
 
-            // block.
-            const block = document.createElement("div");
-            block.classList.add('card-coin');
-            document.querySelector('.coin-section').append(block);
+        // price.
+        const price = document.createElement('p');
+        price.classList.add('price');
+        price.textContent = value.priceCoin;
+        block.append(price);
 
-            // h1.
-            const textBlock = document.createElement('h1');
-            textBlock.classList.add('name-coin');
-            textBlock.textContent = value.nameCoin;
-            block.append(textBlock);
+        // count.
+        const count = document.createElement('p');
+        count.classList.add('count-coin');
+        count.textContent = `quantity ${value.countCoin}`;
+        block.append(count);
 
-            // price.
-            const price = document.createElement('p');
-            price.classList.add('price');
-            price.textContent = value.priceCoin;
-            block.append(price);
+        // plus.
+        const minus = document.createElement('p');
+        minus.classList.add('minus-coin');
+        minus.textContent = '-----';
+        block.append(minus);
 
-            // count.
-            const count = document.createElement('p');
-            count.classList.add('count-coin');
-            count.textContent = `quantity ${value.countCoin}`;
-            block.append(count);
+        // minus.
+        const plus = document.createElement('p');
+        plus.classList.add('plus-coin');
+        plus.textContent = '-----';
+        block.append(plus);
 
-            // plus.
-            const minus = document.createElement('p');
-            minus.classList.add('minus-coin');
-            minus.textContent = '-----';
-            block.append(minus);
-
-            // minus.
-            const plus = document.createElement('p');
-            plus.classList.add('plus-coin');
-            plus.textContent = '-----';
-            block.append(plus);
-
-            // button
-            const deleteButton = document.createElement('button');
-            deleteButton.classList.add('button-delete');
-            deleteButton.textContent = 'DELETE';
-            block.append(deleteButton);
-        }
-    } else {
-        alert('problem');
+        // button
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('button-delete');
+        deleteButton.textContent = 'DELETE';
+        block.append(deleteButton);
     }
 }   
 
@@ -91,14 +80,24 @@ function renderCoin() {
 
         if (!nameAllCoint.includes(document.querySelector(`[date-input="1"]`).value)) {
 
-            coint['nameCoin'] = document.querySelector(`[date-input="1"]`).value;
-            coint['countCoin'] = document.querySelector(`[date-input="2"]`).value;
-            coint['priceCoin'] = document.querySelector(`[date-input="3"]`).value;
-            coint['waitPriceCoin'] = document.querySelector(`[date-input="4"]`).value;
+            const controlP = document.querySelector(`[date-input="1"]`).value == ''
+            || document.querySelector(`[date-input="2"]`).value == ''
+            || document.querySelector(`[date-input="3"]`).value == ''
+            || document.querySelector(`[date-input="4"]`).value == ''
 
-            porfolio.push(coint);
-            nameAllCoint.push(document.querySelector(`[date-input="1"]`).value);
-        }
+
+            if (!controlP) {
+                coint['nameCoin'] = document.querySelector(`[date-input="1"]`).value;
+                coint['countCoin'] = document.querySelector(`[date-input="2"]`).value;
+                coint['priceCoin'] = document.querySelector(`[date-input="3"]`).value;
+                coint['waitPriceCoin'] = document.querySelector(`[date-input="4"]`).value;
+
+                porfolio.push(coint);
+                nameAllCoint.push(document.querySelector(`[date-input="1"]`).value);
+            } else {
+                alert('problem');
+            }
+        } 
 
         form.reset();
 
