38113ccc263e82dce5a506a317c456832069e74d
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-07-22 16:10:50 +0200

    fix but theme

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// data.
-let statusTheme = false;
-
 const PortfolioApp = {
     porfolio: [],
     'renderCoin': renderCoin,
@@ -161,3 +158,48 @@ async function fetchPrices(coinIds) {
         return null
     }
 }
+
+// CHANGE THEME.
+
+// status theme.
+let statusTheme = false;
+
+const changeTheme = function() {
+
+    // change theme.
+    statusTheme = true;
+
+    // change button.
+    const button = document.querySelector('.theme-block');
+    
+    button.addEventListener('click', () => {
+
+        // take all object for change theme.
+        const body = document.body;
+        body.classList.toggle('light');
+
+        // text.
+        const text = document.querySelector('.main-text');
+        text.classList.toggle('main-text-light');
+
+        // form
+        const form = document.querySelector('#form');
+        form.classList.toggle('form-light');
+
+        // theme block.
+        button.classList.toggle('theme-block-light');
+
+        // header.
+        const header = document.querySelector('.header');
+        header.classList.toggle('header-light');
+
+        if (document.querySelector(".card-coin")) {
+            const cardCoin = document.querySelectorAll(".card-coin");
+            cardCoin.forEach(item => {
+                item.classList.toggle('card-coint-light');
+            });
+        }
+    })
+
+};
+changeTheme();
